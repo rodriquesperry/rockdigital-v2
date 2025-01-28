8325bdbbe9971189b4234931e6c236c009b5ee4d
Rodriques <rodriques.perry@gmail.com>
2025-01-28 00:00:37 -0600

    Update: /blog-posts/BlogPosts.component.jsx baseURL fallback

--- components/blog-posts/BlogPosts.component.jsx
+++ components/blog-posts/BlogPosts.component.jsx
@@ -11,8 +11,10 @@ import styles from './blogPosts.module.css';
 const BlogPost = ({ params }) => {
 	const [error, setError] = useState(null);
 	const [posts, setPosts] = useState([]);
-  const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'http://127.0.0.1:1337';
+  const baseURL = process.env.NEXT_PUBLIC_BASE_URL || 'https://rockdigital.agency';
 
+  console.log('baseURL: ', baseURL);
+  
 	useEffect(() => {
 		const getPosts = async () => {
 			await axios
