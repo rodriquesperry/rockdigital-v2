e8cc731f786134692acb37355d0d47a5d3717c23
Rodriques <rodriques.perry@gmail.com>
2025-01-28 18:02:50 -0600

    Add baseURL to PortfolioItem.component.jsx

--- components/portfolio-item/PortfolioItem.component.jsx
+++ components/portfolio-item/PortfolioItem.component.jsx
@@ -4,17 +4,20 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import axios from 'axios';
+import config from '@/config';
 import styles from './portfolioItem.module.css';
 
 const PortfolioItem = () => {
 	const [error, setError] = useState(null);
 	const [portfolioItems, setPortfolioItems] = useState([]);
 
+  const baseURL = config.api || '127.0.0.1:1337';
+
 	useEffect(() => {
 		const getPortfolioItems = async () => {
 			await axios
 				.get(
-					'https://rockdigital.agency/dashboard/api/portfolio-items?populate=*'
+					`${baseURL}/api/portfolio-items?populate=*`
 				)
 				.then((data) => setPortfolioItems(data.data.data))
 				.catch((error) => setError(error));
