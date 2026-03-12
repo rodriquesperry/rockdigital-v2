cac1c8ec383c274b05d0bf2dca6190ab915820d1
Rodriques <rodriques.perry@gmail.com>
2026-03-11 21:31:26 -0500

    Update: Change copy on OG

--- app/opengraph-image.js
+++ app/opengraph-image.js
@@ -99,7 +99,7 @@ export default async function Image() {
 							maxWidth: 680,
 						}}
 					>
-						Premium digital experiences engineered to turn attention into revenue.
+						Premium digital experiences. 
 					</div>
 					<div
 						style={{
