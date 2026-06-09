44a3f6df1a5e72b805c94cf3107f5791d977e4ce
Rodriques <rodriques.perry@gmail.com>
2026-06-09 14:56:31 -0500

    Add case studies link back onto nav

--- components/navigation/navigation.component.jsx
+++ components/navigation/navigation.component.jsx
@@ -14,6 +14,7 @@ import styles from './navigation.module.css';
 const navigationLinks = [
 	{ href: '/about', label: 'ABOUT' },
 	{ href: '/services', label: 'SERVICES' },
+	{ href: '/case-studies', label: 'CASE STUDIES' },
 	{ href: '/blog', label: 'BLOG' },
 	{ href: '/contact', label: 'START THE CONVERSATION' },
 	{
